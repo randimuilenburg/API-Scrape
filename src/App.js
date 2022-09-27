09f464d60a33921cd831cfc45e2c44e577685974
DingoDev <randilea@outlook.com>
2022-09-27 17:08:28 -0600

    updated state names (I don't actually know star wars) and started adding buttons to scroll pages

--- src/App.js
+++ src/App.js
@@ -3,44 +3,70 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { Row, Col, Container } from "react-bootstrap";
 
 function App() {
-  let [spaceships, setSpaceships] = useState([]);
+  let [spacecars, setSpacecars] = useState([]);
 
   useEffect(() => {
     fetch("https://swapi.dev/api/vehicles")
       .then((response) => response.json())
-      .then((data) => setSpaceships(data.results));
+      .then((data) => setSpacecars(data.results));
   }, []);
 
+  // Goes up to 4 pages
   const NextPage = () => {
     useEffect(() => {
-      fetch("https://swapi.dev/api/vehicles/next")
+      fetch("https://swapi.dev/api/vehicles/?page=2")
         .then((response) => response.json())
-        .then((data) => setSpaceships(data.results));
+        .then((data) => setSpacecars(data.results));
+    }, []);
+  };
+
+  const GoBack = () => {
+    useEffect(() => {
+      fetch("https://swapi.dev/api/vehicles")
+        .then((response) => response.json())
+        .then((data) => setSpacecars(data.results));
     }, []);
   };
 
   return (
+    // <Container>
     <div className="App">
       <header className="App-header">
         <Card>
           <Card.Body>
-            {spaceships.map((spaceship) => (
-              <p>{spaceship.name}</p>
+            {spacecars.map((spacecar) => (
+              <p>{spacecar.name}</p>
             ))}
           </Card.Body>
         </Card>
-        <Button
-          className="nextButton"
-          variant="warning"
-          size="medium"
-          onClick={NextPage}
-        >
-          <p>Next</p>
-        </Button>
+        <Row>
+          <Col xs lg="4">
+            <Button
+              className="backButton"
+              variant="warning"
+              size="medium"
+              onClick={GoBack}
+            >
+              <p>Back</p>
+            </Button>
+          </Col>
+          <Col xs lg="4">
+            <Button
+              className="nextButton"
+              variant="warning"
+              size="medium"
+              onClick={NextPage}
+            >
+              <p>Next</p>
+            </Button>
+          </Col>
+        </Row>
       </header>
     </div>
+    /* </Container> */
   );
 }
 
